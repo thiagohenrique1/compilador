d1e39ca575e16a25f64bd369b2288f3b6fbb2c76
thiago <thiago.017henrique@gmail.com>
2017-12-01 20:29:45 -0200

    Analise semantica funcional

--- Semantico.cpp
+++ Semantico.cpp
@@ -31,6 +31,7 @@ void Semantico::executar(){
 				break;
 		}
 	} while(retorno.acao != 'A' && retorno.acao != 'E');
+	output << "}";
 }
 
 item_tabela Semantico::regra_semantica(int reducao, item_tabela nao_terminal, vector<item_tabela> simbolos) {
@@ -56,9 +57,9 @@ item_tabela Semantico::regra_semantica(int reducao, item_tabela nao_terminal, ve
 			if(id.tipo == "literal")
 				output << "scanf(\"%s\"," << id.lexema << ");" << endl;
 			else if(id.tipo == "int")
-				output << "scanf(\"%d\"," << id.lexema << ");" << endl;
+				output << "scanf(\"%d\",&" << id.lexema << ");" << endl;
 			else if(id.tipo == "double")
-				output << "scanf(\"%lf\"," << id.lexema << ");" << endl;
+				output << "scanf(\"%lf\",&" << id.lexema << ");" << endl;
 			else cout << "Erro: Variável não declarada" << endl;
 		} break;
 		case 12: {
@@ -79,6 +80,7 @@ item_tabela Semantico::regra_semantica(int reducao, item_tabela nao_terminal, ve
 		} break;
 		case 13:
 		case 14:
+		case 20:
 		case 15: {
 			nao_terminal.token = simbolos.at(0).token;
 			nao_terminal.lexema = simbolos.at(0).lexema;
@@ -91,26 +93,55 @@ item_tabela Semantico::regra_semantica(int reducao, item_tabela nao_terminal, ve
 			item_tabela LD = simbolos.at(1);
 			item_tabela rcb = simbolos.at(2);
 			if(!id.tipo.empty()){
-				if(id.tipo == LD.tipo)
+				if(id.tipo == LD.tipo || (LD.token == "num" && id.tipo != "literal"))
 					output << id.lexema << " " << rcb.tipo << " " << LD.lexema << ";" << endl;
 				else cout << "Erro: Tipos diferentes para atribuicao" << endl;
 			} else {
 				cout << "Erro: Variável não declarada" << endl;
-			} break;
-		}
+			}
+		} break;
 		case 18: {
 			item_tabela OPRD1 = simbolos.at(2);
 			item_tabela opm = simbolos.at(1);
 			item_tabela OPRD2 = simbolos.at(0);
-			if(OPRD1.tipo == OPRD2.tipo && OPRD1.tipo != "literal") {
-				nao_terminal.lexema = "T"+to_string(++var_temp_num);
-				output << "T" << var_temp_num << " = " <<
-					   OPRD1.lexema << " " << opm.tipo << " " << OPRD2.lexema << endl;
 
-			} else {
+			if((OPRD1.token == "id" && OPRD1.tipo == "literal") || (OPRD2.token == "id" && OPRD2.tipo == "literal")) {
 				cout << "Erro: Operandos com tipos incompativeis" << endl;
+			} else {
+				nao_terminal.lexema = "T"+to_string(++var_temp_num);
+				nao_terminal.token = "id";
+				nao_terminal.tipo = "int";
+				output << "int T" << var_temp_num << " = " <<
+					   OPRD1.lexema << " " << opm.tipo << " " << OPRD2.lexema<<";"<< endl;
 			}
-		}
+		} break;
+		case 19:
+		case 21:
+			nao_terminal.token = simbolos.at(0).token;
+			nao_terminal.lexema = simbolos.at(0).lexema;
+			nao_terminal.tipo = simbolos.at(0).tipo;
+			break;
+		case 23:
+			output << "}" << endl;
+			break;
+		case 24: {
+			item_tabela EXP_R = simbolos.at(2);
+			output << "if("<<EXP_R.lexema<<") {" << endl;
+		} break;
+		case 25: {
+			item_tabela OPRD1 = simbolos.at(2);
+			item_tabela opr = simbolos.at(1);
+			item_tabela OPRD2 = simbolos.at(0);
+
+			if((OPRD1.token == "id" && OPRD1.tipo == "literal") || (OPRD2.token == "id" && OPRD2.tipo == "literal")) {
+				cout << "Erro: Operandos com tipos incompatíveis" << endl;
+			} else {
+				nao_terminal.lexema = "T"+to_string(++var_temp_num);
+				output << "int T" << var_temp_num << " = " <<
+					   OPRD1.lexema << " " << opr.tipo << " " << OPRD2.lexema<<";"<< endl;
+			}
+
+		} break;
 		default:
 			return nao_terminal;
 	}
@@ -119,4 +150,7 @@ item_tabela Semantico::regra_semantica(int reducao, item_tabela nao_terminal, ve
 
 Semantico::Semantico() {
 	output.open("programa.c");
+	output << "#include<stdio.h>" << endl << endl;
+	output << "typedef char literal[256];" << endl;
+	output << "void main(void) {" << endl;
 };
